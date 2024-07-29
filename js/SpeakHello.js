30bdffa4e85fd61c4e1e34b028c157423250eab8
el352 <elilagumbs@gmail.com>
2024-07-29 13:45:10 -0400

    Update SpeakHello.js

--- js/SpeakHello.js
+++ js/SpeakHello.js
@@ -7,3 +7,10 @@
  }
        window.helloSpeaker = helloSpeaker;
 })(window);
+
+console.log("Hello Yaakov");
+console.log("Hello Paul");
+console.log("Hello Frank");
+console.log("Hello Larry");
+console.log("Hello Paula");
+console.log("Hello Laura");
